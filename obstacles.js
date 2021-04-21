e1ad1e074364538014a09e6e4885abba35baa2de
taejin <taejin5314@gmail.com>
2021-04-21 16:54:25 -0400

    function for score

--- obstacles.js
+++ obstacles.js
@@ -7,6 +7,7 @@ class Obstacle {
     this.x = canvas.width;
     this.width = 20;
     this.color = 'hsla(' + hue + ', 100%, 50%, 1)';
+    this.counted = false;
   }
   draw() {
     ctx.fillStyle = this.color;
@@ -15,6 +16,10 @@ class Obstacle {
   }
   update() {
     this.x -= gamespeed;
+    if (!this.counted && this.x < bird.x) {
+      score++;
+      this.counted = true;
+    }
     this.draw();
   }
 }
